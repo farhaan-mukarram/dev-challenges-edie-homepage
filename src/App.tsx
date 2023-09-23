55f9241e7e17bf51433a66517536eb5261b47dee
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-09-23 17:50:17 +0500

    refactor: dummy commit to change folder name

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Container from "./Components/Container";
-import Main from "./Components/Main";
-import Navbar from "./Components/Navbar";
+import Container from "./temp/Container";
+import Main from "./temp/Main";
+import Navbar from "./temp/Navbar";
 
 const App = () => {
   return (
