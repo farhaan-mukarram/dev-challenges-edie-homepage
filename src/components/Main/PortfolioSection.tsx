c7bd8e6239c0dff8978378e31e4bdc88fb887415
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-09-27 01:54:56 +0500

    feat: implement portfolio section

---
+++ src/components/Main/PortfolioSection.tsx
@@ -0,0 +1,58 @@
+import bookingSystemImage from "../../assets/images/booking.png";
+import juiceProductImage from "../../assets/images/juice-product.png";
+import smartHomeImage from "../../assets/images/smarthome.jpg";
+import onboardingImage from "../../assets/images/onboard.png";
+
+const portFolioItems = [
+  {
+    image: smartHomeImage,
+    alt: "smart home dashboard project image",
+    title: "Smart home dashboard",
+    subtitle: "Full stack application",
+  },
+  {
+    image: onboardingImage,
+    alt: "onboarding project image",
+    title: "Onboard application",
+    subtitle: "UX/UI design",
+  },
+  {
+    image: bookingSystemImage,
+    alt: "booking system project image",
+    title: "Booking system",
+    subtitle: "Mobile application",
+  },
+  {
+    image: juiceProductImage,
+    alt: "juice product homepage project image",
+    title: "Juice product homepage",
+    subtitle: "Front End application",
+  },
+];
+
+const PortfolioSection = () => {
+  return (
+    <section className="flex flex-col space-y-3">
+      <h3 className="max-w-md text-4xl font-medium">
+        Good design means good business
+      </h3>
+
+      {/* Projects grid */}
+      <div className="flex flex-wrap gap-x-5 gap-y-36">
+        {portFolioItems.map(({ image, title, subtitle, alt }) => (
+          <div className="flex flex-col basis-1/2 max-w-[34rem] [&:nth-of-type(odd)]:relative [&:nth-of-type(odd)]:top-40 space-y-8">
+            <img src={image} alt={alt} className="rounded-3xl" />
+
+            <div className="flex flex-col space-y-3">
+              <p className="font-light text-sm text-[#828282]">{subtitle}</p>
+              <h4 className="text-2xl font-medium text-[#333333]">{title}</h4>
+            </div>
+          </div>
+        ))}
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioSection;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "./HeroSection";
+import PortfolioSection from "./PortfolioSection";
 import ServicesSection from "./ServicesSection";
 
 const Main = () => {
@@ -6,6 +7,7 @@ const Main = () => {
     <main className="flex flex-col min-h-screen px-12 pt-28 gap-y-52">
       <HeroSection />
       <ServicesSection />
+      <PortfolioSection />
     </main>
   );
 };
