6a99661ad6c42158788242073c469f57f41c8ba5
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-10-03 01:58:14 +0500

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Container from "./components/Container";
+import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Container>
-      <Navbar />
-      <Main />
-    </Container>
+    <>
+      <Container>
+        <Navbar />
+        <Main />
+      </Container>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,70 @@
+import instagramSocial from "../../assets/icons/instagram.svg";
+
+import twitterSocial from "../../assets/icons/twitter.svg";
+import linkedInSocial from "../../assets/icons/linkedin.svg";
+
+const footerNavLinks = ["Home", "Services", "Our Works", "Client", "Contact"];
+
+const socialLinks = [
+  { link: "instagram", image: instagramSocial },
+  { link: "linkedin", image: linkedInSocial },
+  { link: "twitter", image: twitterSocial },
+];
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#100E1D] mt-52">
+      <div className="flex justify-between p-12 mx-auto max-w-7xl">
+        <nav>
+          <ul className="flex flex-col space-y-4 text-white">
+            {footerNavLinks.map((link) => (
+              <li key={link}>{link}</li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="flex flex-col space-y-3">
+          <h2 className="text-4xl font-extrabold text-white font-heebo">
+            Edie
+          </h2>
+
+          <div className="flex items-center space-x-3">
+            {socialLinks.map(({ link, image }) => (
+              <a href="#" key={link}>
+                <img src={image} alt={`${link}-social-icon`} />
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-y-2">
+          <p className="text-white">Want us to contact you?</p>
+
+          <form
+            className="relative flex items-center overflow-hidden"
+            onSubmit={(event) => event?.preventDefault()}
+          >
+            <input
+              required
+              type="email"
+              name="email"
+              id="email"
+              className="bg-[#F2F2F2] rounded-xl py-4 pl-5 w-full"
+              placeholder="Email"
+            />
+
+            <button
+              type="submit"
+              value="Join"
+              className="bg-[#2D9CDB] text-white py-3 px-7 rounded-xl w-max absolute top-1 bottom-1 right-1 my-auto"
+            >
+              Join
+            </button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
