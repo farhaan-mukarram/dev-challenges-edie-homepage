ccdd8ea07e7748b17a55333cee7699019beede37
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2023-10-04 01:37:23 +0500

    feat: add byline to the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,54 +14,79 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="bg-[#100E1D] mt-52">
-      <div className="flex justify-between p-12 mx-auto max-w-7xl">
-        <nav>
-          <ul className="flex flex-col space-y-4 text-white">
-            {footerNavLinks.map((link) => (
-              <li key={link}>{link}</li>
-            ))}
-          </ul>
-        </nav>
+      <div className="flex flex-col p-12 mx-auto max-w-7xl space-y-28">
+        <div className="flex justify-between">
+          <nav>
+            <ul className="flex flex-col space-y-4 text-white">
+              {footerNavLinks.map((link) => (
+                <li key={link}>{link}</li>
+              ))}
+            </ul>
+          </nav>
 
-        <div className="flex flex-col space-y-3">
-          <h2 className="text-4xl font-extrabold text-white font-heebo">
-            Edie
-          </h2>
+          <div className="flex flex-col space-y-3">
+            <h2 className="text-4xl font-extrabold text-white font-heebo">
+              Edie
+            </h2>
 
-          <div className="flex items-center space-x-3">
-            {socialLinks.map(({ link, image }) => (
-              <a href="#" key={link}>
-                <img src={image} alt={`${link}-social-icon`} />
-              </a>
-            ))}
+            <div className="flex items-center space-x-3">
+              {socialLinks.map(({ link, image }) => (
+                <a href="#" key={link}>
+                  <img src={image} alt={`${link}-social-icon`} />
+                </a>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <p className="text-white">Want us to contact you?</p>
+
+            <form
+              className="relative flex items-center overflow-hidden"
+              onSubmit={(event) => event?.preventDefault()}
+            >
+              <input
+                required
+                type="email"
+                name="email"
+                id="email"
+                className="bg-[#F2F2F2] rounded-xl py-4 pl-5 w-full"
+                placeholder="Email"
+              />
+
+              <button
+                type="submit"
+                value="Join"
+                className="bg-[#2D9CDB] text-white py-3 px-7 rounded-xl w-max absolute top-1 bottom-1 right-1 my-auto"
+              >
+                Join
+              </button>
+            </form>
           </div>
         </div>
 
-        <div className="flex flex-col gap-y-2">
-          <p className="text-white">Want us to contact you?</p>
-
-          <form
-            className="relative flex items-center overflow-hidden"
-            onSubmit={(event) => event?.preventDefault()}
-          >
-            <input
-              required
-              type="email"
-              name="email"
-              id="email"
-              className="bg-[#F2F2F2] rounded-xl py-4 pl-5 w-full"
-              placeholder="Email"
-            />
-
-            <button
-              type="submit"
-              value="Join"
-              className="bg-[#2D9CDB] text-white py-3 px-7 rounded-xl w-max absolute top-1 bottom-1 right-1 my-auto"
+        <p className="font-medium text-center text-[#BDBDBD]">
+          created by{" "}
+          <span className="font-bold">
+            <a
+              href="https://github.com/farhaan-mukarram/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
             >
-              Join
-            </button>
-          </form>
-        </div>
+              farhaan-mukarram
+            </a>
+          </span>
+          {" - "}
+          <a
+            href="https://devchallenges.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            devChallenges.io
+          </a>
+        </p>
       </div>
     </footer>
   );
